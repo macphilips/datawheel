3930c9cf2d38531397f579e8d22d374ea268e680
Morolari Titilope <tmorolari@gmail.com>
2020-07-10 06:45:16 +0100

    add integration test

---
+++ src/test/java/com/datawheel/demo/controller/CounterCrtlTest.java
@@ -0,0 +1,112 @@
+package com.datawheel.demo.controller;
+
+import com.datawheel.demo.DemoApplication;
+import com.datawheel.demo.domain.Counter;
+import com.datawheel.demo.repository.CounterRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = DemoApplication.class)
+@AutoConfigureMockMvc
+@Transactional
+class CounterCrtlTest {
+    @Autowired
+    private CounterRepository counterRepository;
+
+    @Autowired
+    private MockMvc restUserMockMvc;
+
+    public Counter createCounter() {
+        Counter counter = new Counter();
+        counter.setCount(12L);
+        return (counter);
+    }
+
+    @BeforeEach
+    void cleanUp() {
+        counterRepository.deleteAll();
+    }
+
+    @Test
+    void getCountShouldThrow() throws Exception {
+        restUserMockMvc.perform(get("/api/counter")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void getCounter() throws Exception {
+        Counter counter = createCounter();
+        counterRepository.saveAndFlush(counter);
+        restUserMockMvc.perform(get("/api/counter")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.totalCount").value(counter.getCount()));
+
+    }
+
+    @Test
+    void createCounters() throws Exception {
+        // should create a new counter
+        restUserMockMvc.perform(post("/api/counter")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{ \"totalCount\": 6 }"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.totalCount").value(6));
+    }
+
+    @Test
+    void createCounterShouldThrowBadRequestError() throws Exception {
+        restUserMockMvc.perform(post("/api/counter")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{ \"totalCount\": 56,  \"id\": 8 }"))
+            .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void updateExistingCounter() throws Exception {
+        Counter counter = createCounter();
+        counter = counterRepository.saveAndFlush(counter);
+
+        restUserMockMvc.perform(put("/api/counter")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{ \"totalCount\": 56, \"id\": " + counter.getId() + " }"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.totalCount").value(56));
+
+        assertThat(counterRepository.getOne(counter.getId()).getCount()).isEqualTo(56L);
+    }
+
+    @Test
+    void updateCounterShouldThrowBadRequestError() throws Exception {
+
+        Counter counter = createCounter();
+        counter = counterRepository.saveAndFlush(counter);
+
+        restUserMockMvc.perform(put("/api/counter")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{ \"totalCount\": 6 }"))
+            .andExpect(status().isBadRequest());
+
+        assertThat(counterRepository.getOne(counter.getId()).getCount()).isEqualTo(counter.getCount());
+
+    }
+
+}
